2d6658cffd31a0c40edf7ef85aaca154c2f677e8
spi8as7 <spi8as7@hotmail.gr>
2022-06-29 16:05:28 -0700

    Iniital Commit
    - MongoDB
    - Node.js backend app
    - POST /search utilizes twitter API to search for a specific keyword and update local db
    - GET /search/all get all search requests per keyword

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+const {TwitterApi} = require('twitter-api-v2')
+const dotenv  = require('dotenv').config(); 
+const mongoose = require('mongoose');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.ACCESS_TOKEN_KEY,
+    accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+mongoose.connect("mongodb://localhost:27017/twitter");
+
+const twittSchema = {
+    created_at: String,
+    author_id: String,
+    public_metrics: { type : Array , "default" : [] },
+    twitt_text: String,
+    twitt_id: String
+}
+
+const searchSchema = {
+    keyword: String,
+    search_result : { type : Array , "default" : [] },
+    total_results: Number,
+    total_likes: Number,
+    timestamp: String
+};
+
+const Search = mongoose.model("Search", searchSchema);
+
+function calculateTotalLikes(tweets) {
+    let total_likes = 0;
+    tweets.forEach(function(tweet) {
+        total_likes += tweet['public_metrics']['like_count']
+    });
+    return total_likes
+}
+
+app.post('/search',  async (req, res) => {
+    let {keyword,start_time,end_time,timestamp} = req.body ;
+
+    const tweet_fields = ['author_id','created_at','id','public_metrics']
+    const jsTweets = 
+        await client.v2.search(keyword,
+        {
+            'start_time':start_time,
+            'max_results' : 100, //limitation due to student auth
+            'end_time':end_time,
+            'tweet.fields':tweet_fields
+        });    
+
+    //timestamp format    RFC 3339
+    const data = jsTweets['_realData']['data'] 
+
+    const search_schema = new Search ({
+        keyword: keyword,
+        search_result: data,
+        total_results: data.length,
+        total_likes: calculateTotalLikes(data),
+        timestamp: new Date().toISOString() //should be replaced by request timestamp
+    });
+
+    search_schema.save();
+    
+    return res.json(200);
+})
+
+app.get('/search/all', async (req, res) => {
+    //db.getCollection('searches').find({})
+    var search_result = [];
+    Search.find({}, function(err, searches) {
+        searches.forEach(function(search) {
+            search_result.push({
+                keyword:search['keyword'],
+                search_result:search['search_result'],
+                total_results:search['total_results'],
+                total_likes:search['total_likes'],
+                timestamp:search['timestamp']
+            })
+        });
+        return res.json(search_result);
+    });
+})
+
+var server = app.listen(8082, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
